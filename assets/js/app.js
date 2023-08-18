d15d637fdc726cbd8a7c6d3ac3990bf20504bc00
Alla Romasheva <alla@romasheva.com>
2023-08-19 02:19:07 +0300

    update portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -131,10 +131,18 @@ var html = document.documentElement;
 var container = document.scrollingElement;
 var scroll = 0;
 
+var idle = function idle(cb, ttl) {
+  clearTimeout(cb.idle);
+  cb.idle = setTimeout(cb, ttl);
+};
+
 var scroller = function scroller() {
   var scrollTop = container.scrollTop;
   var progress = parseFloat(scrollTop / ((html.scrollHeight - html.clientHeight) / 100)).toFixed(2);
-  html.style.setProperty('--progress', String(progress));
+  idle(function () {
+    html.style.setProperty('--progress', String(progress));
+  }, 300);
+  html.style.setProperty('--window-height', window.innerHeight + 'px');
   html.classList.toggle('scroll', scrollTop > 0);
   html.classList.toggle('scroll-bottom', scrollTop > 0 && scroll < scrollTop);
   html.classList.toggle('scroll-top', scrollTop > 0 && scroll > scrollTop);
--- assets/src/component/scroll.js
+++ assets/src/component/scroll.js
@@ -5,10 +5,20 @@ const container = document.scrollingElement;
 
 let scroll = 0;
 
+const idle = (cb,ttl) => {
+    clearTimeout(cb.idle)
+    cb.idle = setTimeout(cb,ttl)
+}
+
 const scroller = () => {
     let scrollTop = container.scrollTop;
     let progress  = parseFloat(scrollTop / ((html.scrollHeight - html.clientHeight) / 100) ).toFixed(2);
-    html.style.setProperty('--progress',String(progress));
+
+    idle(()=>{
+        html.style.setProperty('--progress',String(progress));
+    },300)
+
+    html.style.setProperty('--window-height', window.innerHeight + 'px')
     html.classList.toggle('scroll', scrollTop > 0);
     html.classList.toggle('scroll-bottom', scrollTop > 0 && (scroll < scrollTop));
     html.classList.toggle('scroll-top', scrollTop > 0 && (scroll > scrollTop));
